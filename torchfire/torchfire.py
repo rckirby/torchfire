0576000cf2700418478c564a67212f15be1e1e7f
Re-GeorX <jmarchenaster@gmail.com>
2022-09-26 21:27:43 -0500

    Changes to allow compatibility with the method backward()

--- torchfire/torchfire.py
+++ torchfire/torchfire.py
@@ -1,3 +1,6 @@
+import os
+os.environ['OMP_NUM_THREADS']='1'
+
 import torch
 import firedrake
 import firedrake_adjoint  # noqa
@@ -12,18 +15,20 @@ def fd_to_torch(fd_callable, templates, classname):
     the static forward and backward methods in terms of fecr."""
 
     def forward(ctx, *inputs):
+        np_inputs = [i.numpy().astype(np.float64()) for i in inputs]
         np_output, fd_output, fd_input, tape = evaluate_primal(
-            fd_callable, templates, *inputs)
+            fd_callable, templates, *np_inputs)
         ctx.save_for_backward(*inputs)
         ctx.stuff = (np_output, fd_output, fd_input, tape)
-
-        return np_output
+        return torch.tensor(np_output, requires_grad=True)
 
     def backward(ctx, grad_output):
-        # inputs = ctx.saved_tensors
+        #inputs, = ctx.saved_tensors
         np_output, fd_output, fd_input, tape = ctx.stuff
         g = np.ones_like(np_output)
         vjp_out = evaluate_pullback(fd_output, fd_input, tape, g)
+        t_output = [torch.tensor(i, requires_grad=True) for i in vjp_out]
+        return tuple(t_output)
         # todo: figure out shape of grad_output...
 
     bases = (torch.autograd.Function,)
@@ -44,12 +49,17 @@ def assemble_firedrake(u, kappa0, kappa1):
     return J
 
 
+#if __name__ == "__main__":
 mesh = firedrake.UnitSquareMesh(3, 2)
 V = firedrake.FunctionSpace(mesh, "P", 1)
 templates = (firedrake.Function(V),
              firedrake.Constant(0.0), firedrake.Constant(0.0))
-inputs = (np.ones(V.dim()), np.ones(1) * 0.5, np.ones(1) * 0.6)
+inputs = (torch.ones(V.dim(), requires_grad=True), torch.ones(1, requires_grad=True) * 0.5, torch.ones(1, requires_grad=True) * 0.6)
 
 bob = fd_to_torch(assemble_firedrake, templates, "bob")
 
-bob.forward(None, *inputs)
+J_1 = bob.apply
+result = J_1(*inputs)
+result.backward()
+
+
