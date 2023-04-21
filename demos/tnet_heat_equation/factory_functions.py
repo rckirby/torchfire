attempts to fix gradient issue

demos/tnet_heat_equation/factory_functions.py
@@ -90,7 +90,7 @@ def factory_mcLoss(args: dict) -> tuple[str, Callable]:
 
         # compute L2 loss 
         loss = torch.mean(torch.square(u - u_obs_true))
-        loss.backward()
+        loss.backward(retain_graph=True)
         return {'val': loss, 'grad': k.grad}
     return "mcLoss", func
 

demos/tnet_heat_equation/tnet_heat_equation.py
@@ -21,10 +21,10 @@ repeat_fac = 1  # Keep it 1 for now!
 
 learning_rate = 1e-3
 batch_size = num_train
-epochs = 100
+epochs = 5
 neurons = 5000
 
-alpha = 8e3  # this value is the best for noise level of 0.005
+alpha = 0#8e3  # this value is the best for noise level of 0.005
 noise_level = 0.005
 
 
@@ -137,7 +137,7 @@ class NeuralNetwork(nn.Module):
 
             loss_mc += torch.mean(torch.square(u_ - u_obs_true_)) * num_observation / num_train
 
-        return loss_mc
+        return loss_mc[0]
 
 
 model = NeuralNetwork().to(device)
@@ -152,7 +152,7 @@ def train_loop(model, optimizer, z, u_train_true, alpha):
 
     loss_mc = model.SolverTorch(kappa, u_obs_batch)
     loss = loss_ml + alpha * loss_mc
-    print(f"ML loss: {loss_ml:1.4e},  MC loss: {loss_mc[0]:1.4e}", flush=True)
+    print(f"ML loss: {loss_ml:1.4e},  MC loss: {loss_mc:1.4e}", flush=True)
     
     optimizer.zero_grad()
     loss.backward()
@@ -166,7 +166,7 @@ def test_loop(model, z_test, u_test_true):
         z_test_pred, _ = model(u_test_true)
         kappa_pred = torch.einsum('ij,bj -> bi', torch.matmul(Eigen, Sigma).float(), z_test_pred)
         kappa_true = torch.einsum('ij,bj -> bi', torch.matmul(Eigen, Sigma).float(), z_test.float())
-
+    
     return torch.mean(
         torch.linalg.vector_norm(kappa_pred - kappa_true, dim=-1) ** 2 / torch.linalg.vector_norm(kappa_true,
                                                                                                   dim=-1) ** 2)
@@ -187,7 +187,7 @@ for t in range(epochs):
     test_u_acc = test_loop(model, test_Parameters, test_Observations)
 
     str_test_u_acc = numpy_formatter(test_u_acc.cpu().detach().numpy())
-    str_train_loss = numpy_formatter(train_loss.cpu().detach().numpy()[0])
+    str_train_loss = numpy_formatter(train_loss.cpu().detach().numpy())
 
     if t % 1 == 0:
         print(f"Epoch {t + 1}\n-------------------------------")

src/torchfire/mpi_function.py
@@ -37,8 +37,8 @@ class MpiReduceTorchFunction(torch.autograd.Function):
         """
         mpi_size = comm.Get_size()
         available_procs = [i for i in range(1, mpi_size)]
-        solutions = []
         # this needs changed to be general. Hardcoding now to test if it works first.
+        solutions = torch.zeros((data[1].shape[0]), requires_grad=True)
         grads = torch.zeros_like(data[1], requires_grad=True)
 
         # initialize scenarios left to the total number of scenarios to run
@@ -52,7 +52,7 @@ class MpiReduceTorchFunction(torch.autograd.Function):
             
             if s.tag == FLAGS.RUN_FINISHED:
                 outputs = comm.recv()
-                solutions.append(outputs['val'])
+                solutions[outputs['index']] = outputs['val']
                 grads[outputs['index'], :] = outputs['grad']
                 #grads.append(outputs['grad'])
                 scenarios_left -= 1
@@ -76,16 +76,12 @@ class MpiReduceTorchFunction(torch.autograd.Function):
             elif len(available_procs) == 0:
                 # if there are no processes available, wait a bit
                 sleep(0.1)
-                
-        reduced_sol = torch.zeros_like(solutions[0], requires_grad=True)
-        for sol in solutions:
-            reduced_sol += sol
         
         # we assume that the only gradients that matter are the gradients
         # of the output w.r.t. the input
         #print(grads, flush=True)
         ctx.save_for_backward(None, grads)
-        return reduced_sol
+        return solutions
         
     @staticmethod
     def backward(ctx, grad_outputs):